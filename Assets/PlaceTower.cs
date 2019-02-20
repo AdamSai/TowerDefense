247850a96d9b2e8b683be493661f0e1b1dff1b77
Adam Saidane <adamsaidane@hotmail.com>
2019-02-20 17:26:19 +0100

    Only spawn a "tower" if the position is free. Delete tower with right click.

--- Assets/PlaceTower.cs
+++ Assets/PlaceTower.cs
@@ -6,19 +6,20 @@ public class PlaceTower : MonoBehaviour
 {
     public GameObject tower;
     public LayerMask layermask;
-    private GameObject previewBox;
+    public GameObject previewBox;
+    private GameObject _previewBox;
     private Vector3 _oldPos;
     private float _oldX;
     private float _oldZ;
     // Start is called before the first frame update
     void Start()
     {
-        previewBox = Instantiate(tower, Vector3.zero, Quaternion.identity);
-        previewBox.SetActive(true);
-        _oldPos = previewBox.transform.position;
+        _previewBox = Instantiate(previewBox, Vector3.zero, Quaternion.identity);
+        _previewBox.SetActive(true);
+        _oldPos = _previewBox.transform.position;
         _oldX = _oldPos.x;
         _oldZ = _oldPos.z;
-        
+
     }
 
     // Update is called once per frame
@@ -30,26 +31,44 @@ public class PlaceTower : MonoBehaviour
 
         if (Physics.Raycast(ray, out RaycastHit hit, 1000f, layermask))
         {
-            //print(Mathf.Round(hit.point.x) % 2);
-            //if(Mathf.Round(hit.point.x) % 2 == 0)
-            //{
-                _oldX = Mathf.Round(hit.point.x);
-            //}
-            //if (Mathf.Round(hit.point.z) % 2 == 0)
-            //{
-                _oldZ = Mathf.Round(hit.point.z);
-            //}
-            var newPos = new Vector3(_oldX, hit.point.y, _oldZ);
-            previewBox.transform.position = newPos;
-            if (Input.GetButton("Fire1"))
+            if (hit.transform.gameObject.layer != 10)
             {
-                var box = Instantiate(tower, newPos, Quaternion.identity);
-                box.SetActive(true);
-                Debug.DrawLine(Camera.main.transform.position, hit.point);
+                
+                var newPos = CalculateNewPosition(hit);
+                _previewBox.transform.position = newPos;
+                if (Input.GetButton("Fire1"))
+                {
+                    CreateTower(hit, newPos);
+                }
+            }
+            if (Input.GetButton("Fire2"))
+            {
+                DeleteTower(hit);
             }
         }
 
 
 
+    }
+
+    private Vector3 CalculateNewPosition(RaycastHit hit)
+    {
+        _oldX = Mathf.Round(hit.point.x);
+        _oldZ = Mathf.Round(hit.point.z);
+        return new Vector3(_oldX, hit.point.y, _oldZ);
+    }
+
+    private void CreateTower(RaycastHit hit, Vector3 newPos)
+    {
+        var box = Instantiate(tower, newPos, Quaternion.identity);
+        box.SetActive(true);
+        Debug.DrawLine(Camera.main.transform.position, hit.point);
+    }
+
+    private static void DeleteTower(RaycastHit hit)
+    {
+        print(hit.transform.gameObject.layer);
+        if (hit.transform.gameObject.layer != 9)
+            Destroy(hit.transform.gameObject);
     }
 }

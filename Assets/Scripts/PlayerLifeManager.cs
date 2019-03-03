ad5f63c7549ce43d3e93a111162ccc819ce2a354
Adam Saidane <adamsaidane@hotmail.com>
2019-03-03 18:50:41 +0100

    Game ends when life reaches 0. Added restart button to restart the game when you lose.

--- Assets/Scripts/PlayerLifeManager.cs
+++ Assets/Scripts/PlayerLifeManager.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerLifeManager : MonoBehaviour
 {
     public int life = 30;
     public TextMeshProUGUI lifeText;
+    public TextMeshProUGUI gameovertext;
+    public GameObject button;
     // Start is called before the first frame update
     void Start()
     {
         lifeText.text = $"Lives: {life.ToString()}";
+        Time.timeScale = 1;
     }
 
 
@@ -18,5 +22,17 @@ public class PlayerLifeManager : MonoBehaviour
     {
         life--;
         lifeText.text = $"Lives: {life.ToString()}";
+        if(life <= 0)
+        {
+            button.SetActive(true);
+            gameovertext.gameObject.SetActive(true);
+            gameovertext.text = "Game over!";
+            Time.timeScale = 0;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
